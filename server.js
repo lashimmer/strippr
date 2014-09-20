121da958f1865c1aec0cd2e6340f176ab9af897c
shanna wang <lashimmer@gmail.com>
2014-09-20 16:27:06 -0400

    a

--- server.js
+++ server.js
@@ -28,14 +28,12 @@ var router = express.Router(); 				// get an instance of the express Router
 // middleware to use for all requests
 router.use(function(req, res, next) {
 	// do logging
-	console.log('Something is happening.');
 	next(); // make sure we go to the next routes and don't stop here
 });
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	console.log("it works");
     res.sendfile(__dirname + '/views/index.html');
 });
 
