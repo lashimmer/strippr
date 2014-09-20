c75f6eba898811aa2a021b9644c1d56f2d3def5e
reetah <rita.lu410@gmail.com>
2014-09-20 12:42:31 -0400

    changed port

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,20 @@
 var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
 
-var UserSchema   = new Schema({
-	username: String,
-	password: String,
-	email: String,
-	description: String,
-	Subscription: Array,
-	Favourites: Array
+var db = mongoose.Connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+	var Schema       = mongoose.Schema;
+	var UserSchema   = new Schema({
+		username: String,
+		password: String,
+		email: String,
+		description: String,
+		Subscription: Array,
+		Favourites: Array
+	});
+
+	module.exports = mongoose.model('User', UserSchema);
 });
 
-
-module.exports = mongoose.model('User', UserSchema);
+mongoose.connect('mongodb://');
