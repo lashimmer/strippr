4008e5c7aac58cae808dd7c7bfd7cdcb853b6a88
shanna wang <lashimmer@gmail.com>
2014-09-20 13:30:07 -0400

    .

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ router.use(function(req, res, next) {
 	next(); // make sure we go to the next routes and don't stop here
 });
 
-app.engine('jade', require('jade').__express);
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
