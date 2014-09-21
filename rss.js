19f644bd45cc1970b37dcd5eb615cda873356c4e
reetah <rita.lu410@gmail.com>
2014-09-21 00:42:24 -0400

    xkcd feed up

--- rss.js
+++ rss.js
@@ -1,11 +1,19 @@
 module.exports = {
   execute: function () {
+
+  	// add references to models
+	var User     = require('./app/models/user');
+	var Comic     = require('./app/models/comic');
+	var Strip     = require('./app/models/strip');
+  	var http = require('http');
 	var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
 	$.support.cors = true;
+
+	var DOMParser = require('xmldom').DOMParser;
     // polls the the rss feeds regularly
     // 2 hours
-	//var interval = 7200000;
-	var interval = 5000;
+	var interval = 7200000;
+	//var interval = 2000;
 	setInterval(function(){
     	pollAll(function(result){
    	 	});
@@ -20,19 +28,74 @@ module.exports = {
 
 	function pollXKCD()
 	{
-		console.log("polling XKCD")
-		$.ajax({
-        	url: 'http://xkcd.com/rss.xml',
-        	type: 'GET',
-        	contentType: 'text/xml',
-        	dataType: 'xml',
-        	success : function(data) {
-       		console.log("SUCCESS");
-   			},
-   			error : function(data) {
-       		console.log(data);
-   			}
-    	});	
+		console.log("polling XKCD");
+		var url = "http://xkcd.com/rss.xml"
+		var req = http.get(url, function(res) {
+		  // save the data
+		  var xml = '';
+		  res.on('data', function(chunk) {
+		    xml += chunk;
+		  });
+
+		  res.on('end', function() {
+		    // parse xml: remove xml version first
+		    //xml = xml.substr(xml.indexOf('>') + 1, xml.length);
+		    xml = xml.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+		    xml = xml.replace('&lt;', '<').replace('&gt;', '>');
+		    //console.log(xml);
+
+		    var doc = new DOMParser().parseFromString(xml);
+			items = doc.getElementsByTagName('item');
+			for (i = 0; i < items.length; i++)
+		  	{
+		  		var strip = new Strip();
+		  		strip.comic = "http://xkcd.com";
+		  		strip.likes = 0;
+		  		var subitems = items[i].childNodes;
+		  		for (j = 0; j < subitems.length; j++){
+		  			var key = subitems[j].nodeName;
+		  			var value = subitems[j].childNodes[0].nodeValue;
+					switch (key) {
+						case "title":
+							strip.title = value;
+							break;
+						case "link":
+							strip.link = value;
+							break;
+						case "description":
+							strip.img = subitems[j].childNodes[0].attributes[0].nodeValue;
+							console.log(strip.img);
+							strip.description = subitems[j].childNodes[0].attributes[1].nodeValue;
+							break;
+						case "pubDate":
+							strip.date = value;
+							break;
+					}
+		  		}
+		  	var stripExists = false;
+		  	Strip.find({ img: strip.img}, function(err, strip) {
+				if (err) {
+					console.log(err);					
+				}
+				if (strip != null) {
+					stripExists = true;
+				}
+			});
+			console.log(stripExists);
+		  	if (stripExists == false) {
+		  		// strip doesn't already exist in database, insert
+		  		console.log("doesn't exist!");
+		  		strip.save();
+		  	}
+
+		  	}
+		    //console.log(items);    
+		  });
+		});
+
+		req.on('error', function(err) {
+		  // debug error
+		});	
     }
 }
 }
