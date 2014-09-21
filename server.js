75c1b57dcd574b272062de39a19fa5ae70ec659c
shanna wang <lashimmer@gmail.com>
2014-09-21 07:32:03 -0400

    favicon

--- server.js
+++ server.js
@@ -35,9 +35,7 @@ router.use(function(req, res, next) {
 	next(); // make sure we go to the next routes and don't stop here
 });
 
-var favicon = require('serve-favicon');
-app.use(favicon(__dirname + '/public/img/favicon.ico'));
-
+app.use(favicon(path.join(__dirname,'public','imag','favicon.ico'));
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
