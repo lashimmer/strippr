907338707eb06f9ab09e5b749b6a989fcf905148
reetah <rita.lu410@gmail.com>
2014-09-26 17:22:04 -0400

    freakin async

--- crawler.js
+++ crawler.js
@@ -5,6 +5,8 @@ module.exports = {
 	var User     = require('./app/models/user');
 	var Comic     = require('./app/models/comic');
 	var Strip     = require('./app/models/strip');
+
+
   	var http = require('http');
 	// var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
 	// $.support.cors = true;
@@ -13,41 +15,90 @@ module.exports = {
     // polls the the rss feeds regularly
     // 2 hours
 	//var interval = 7200000;
-	var interval = 2000;
-	setInterval(function(){
-    	pollAll(function(result){
-   	 	});
-	}, interval);
+	// var interval = 2000;
+	// setInterval(function(){
+ //    	pollAll(function(result){
+ //   	 	});
+	// }, interval);
+	//pollAll();
 
 	function pollAll()
 	{
-		//pollComic("http://xkcd.com/archive");
+		pollXKCD();
 	}
 
-	function pollComic(url)
+	function pollXKCD()
 	{
-		function download(url, callback) {
-  			http.get(url, function(res) {
-    		var data = "";
-    		res.on('data', function (chunk) {
-      		data += chunk;
-    		});
-    		res.on("end", function() {
-      		callback(data);
-    		});
-  			}).on("error", function() {
-    		callback(null);
-  			});
-}
 		console.log("polling XKCD"); 
-
+		var baseurl = "http://xkcd.com"
+		var url = baseurl + "/archive/"
 		download(url, function(data) {
-  		if (data) {
-    	console.log(data);
-  		}
-  		else console.log("error");  
+			var strip = new Strip();
+	  		if (data) {
+	    		var $$ = cheerio.load(data);
+	    		$$("a").each(function(i, e) { 
+	    			if (e.attribs.title != null) {
+	    				//strip.link = baseurl + e.attribs.href;
+	    				//strip.date = e.attribs.title;
+
+	    				// now scrape individual links	
+				    	download(baseurl + e.attribs.href, function(data2) {
+	    					//console.log("insited nested");
+	  						if (data2) {
+	    					var $ = cheerio.load(data2);
+	    					$("img").each(function(i, e) { 
+	    						if (e.attribs.title != null) {
+	    							strip.img = e.attribs.src;
+	    							strip.description = e.attribs.title;
+	    							strip.title = e.attribs.alt;
+	    							strip.likes = 0;
+	    							strip.comic = "xkcd";
+	    							strip.link = baseurl + e.attribs.href;
+
+
+				      			Strip.find({ link: strip.link}, function(err, returnedStrip) {
+									if (err) {
+										console.log("err");					
+									}
+									if (returnedStrip != null) {
+										console.log("NOT EXIST");
+										console.log(strip);
+										//strip.save();
+										console.log("saved");
+									}
+									else {
+										console.log("EXISTS");
+									}
+								});			
+	    						}
+	      						});
+  							}
+  							else console.log("error");  
+						});
+    				}
+      			});
+
+  			}
+
+  			else console.log("error");  
 		});
     }
+
+// common helper functions
+	function download(url, callback) {
+  		http.get(url, function(res) {
+    		var data = "";
+    		res.on('data', function (chunk) {
+      			data += chunk;
+    		});
+    		res.on("end", function() {
+      			callback(data);
+    		});
+  			}).on("error", function() {
+    			callback(null);
+  			});
+	}
+
 }
 }
 
