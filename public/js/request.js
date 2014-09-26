dce85f89a9d7e2c7175569f894e0b205fe9f9e83
shanna wang <lashimmer@gmail.com>
2014-09-26 15:35:54 -0400

    update frontend

--- public/js/request.js
+++ public/js/request.js
@@ -14,21 +14,26 @@ var load=function(date, number, username) {
 var renderStrip=function(data){
 	var content="";
 	for (var i = 0; i < data.length; i++) {
-		if (data[i] == "null") break;
-		content +=
-		'<div class="strip">'+
-        '<div class="head">'+
-       	data[i].title+
-         ' <div class="likes">'+
-          '  <img src="../public/img/heart.png">'+
-          data[i].likes +
-          '</div>'+
-        '</div>'+
-        '<div class="pic">'+
-         ' <img src="'+data[i].img+'">'+
-        '</div>'+
-      '</div>'
-	};
-	$('#content').html("");
-	$('#content').append(content);
+		if (data[i] == "null") {
+      break;
+    }
+    else {
+      content +=
+        '<div class="strip">'+
+            '<div class="head">'+
+            data[i].title+
+             ' <div class="likes">'+
+              '  <img src="../public/img/heart.png">'+
+              data[i].likes +
+              '</div>'+
+            '</div>'+
+            '<div class="pic">'+
+             ' <img src="'+data[i].img+'">'+
+            '</div>'+
+          '</div>'
+      };
+      $('#content').html("");
+      $('#content').append(content);
+    }
+		
 }
