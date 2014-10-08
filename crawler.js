7476f4c95442752b837fb5f68aecf5eaf810dcd8
reetah <rita.lu410@gmail.com>
2014-10-07 20:52:06 -0400

    getstripsbydate now uses current date

--- crawler.js
+++ crawler.js
@@ -35,7 +35,7 @@ module.exports = {
 			}
 		});
 	}
-
+// uses today's date
 	function pollGeneric(comic)
 	{
 		var url = comic.website + comic.archive;
@@ -58,7 +58,8 @@ module.exports = {
 			    				strip.date = e.attribs.title;
 			    			}
 			    			else {
-
+			    				// uses the date & time right now if it cannot find date
+			    				strip.date = new Date();
 			    			}
 				    		// invokes method to scrape individual links
 				    		//console.log(strip.link);
@@ -100,40 +101,67 @@ module.exports = {
       		});
 		});
     }
-
-	function pollXKCD()
+// makes up a date that enforce the rough chronological ordering
+    function pollGenericPast(comic)
 	{
-		var baseurl = "http://xkcd.com";
-		var url = baseurl + "/archive/";
+		var url = comic.website + comic.archive;
 		download(url, function(data) {
 	    	var $$ = cheerio.load(data);
 	    	$$("a").each(function(i, e) { 
 	    		function innerLoop(callback, strip, saveFunction, stripSave) {
-		    		if (e.attribs.title != null) {
-		    			var strip = new Strip();
-		    			strip.link = baseurl + e.attribs.href;
-		    			strip.date = e.attribs.title;
-		    			// invokes method to scrape individual links
-		    			callback(strip, saveFunction, stripSave);
+		    		var strip = new Strip();
+		    		if (e.attribs != null) {
+		    			if (e.attribs.href != null) {
+		    				// deals with absolute and relative paths
+		    				if (e.attribs.href.toString().indexOf(comic.trunc) > -1) {
+			    				strip.link = e.attribs.href;
+			    			}
+			    			else {
+				    			strip.link = comic.website + e.attribs.href;
+			    			}
+			    			// try to pull date: at least xkcd has it in the title
+			    			if (e.attribs.title != null) {
+			    				strip.date = e.attribs.title;
+			    			}
+			    			else {
+			    				
+			    			}
+				    		// invokes method to scrape individual links
+				    		//console.log(strip.link);
+			    			callback(strip, saveFunction, stripSave);
+		    			}
 		    		}
 	    		}
 	    		function innerLoopFunction(strip, saveFunction, stripSave) {
-    				download(baseurl + e.attribs.href, function(data2) {
-    					var $ = cheerio.load(data2);
-    					$("img").each(function(i, e) { 
-    						if (e.attribs.title != null) {
-    							strip.img = e.attribs.src;
-    							strip.description = e.attribs.title;
-    							strip.title = e.attribs.alt;
-    							strip.likes = 0;
-    							strip.comic = "xkcd";
-    							// pass to save function to check if it already exists
-								saveFunction(strip, stripSave);
-    						}
-      					});
-					});
-   				}
-	    		
+	    			// TODO: use less try catch statements
+	    			try { // this one catches the mailto illegal actions
+	    				download(strip.link, function(data2) {
+		    				try { // this one catches the parent null errors
+			    				var $ = cheerio.load(data2);
+			    				$("img").each(function(i, e) { 
+			    					if (e.attribs != null) {
+			    						// try looking for images with titles first
+			    						if (e.attribs.title != null) {
+					    					strip.img = e.attribs.src;
+					    					strip.description = e.attribs.title;
+					    					strip.title = e.attribs.alt;
+					    					strip.likes = 0;
+					    					strip.comic = comic.trunc;
+					    					console.log(strip);
+					    					// save goes inside if/else loop to avoid saving things that don't match
+											//saveFunction(strip, stripSave);
+			    						}
+			    					}
+			      				});	    						
+		    				}
+		    				catch (err) {
+		    					//console.log(err);
+		    				}
+						});
+	    			}
+	    			catch (error) {
+	    			}	    			
+   				}    		
 	    	innerLoop(innerLoopFunction, null, findAndSave, saveIt);
       		});
 		});
